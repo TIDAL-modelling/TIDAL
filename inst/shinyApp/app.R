c7d6b66c79ddc074d22bb2348626e167c8ba6721
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2023-01-26 15:22:44 +0000

    add some basic documentation

--- inst/shinyApp/app.R
+++ inst/shinyApp/app.R
@@ -7,9 +7,34 @@ appDir <- system.file("shinyApp", package = "trajMods")
 source(paste0(appDir, "/modules.R"))
 
 # Increase maximum file size that can be uploaded
-options(shiny.maxRequestSize = 20*1024^2)
+options(shiny.maxRequestSize = 30*1024^2)
 
 # User interface
+welcome_page <- tabPanel(
+  title = "Overview",
+  fluidPage(
+  h1("Multilevel Modelling of Longitudinal Data"),
+  p("The aim is for this digital tool to facilitate trajectories work and remove barriers to implementing longitudinal research to researchers without specialist statistical backgrounds. The following pages guide trajectory modelling and capture clinically meaningful features from mental health trajectories for specific individuals and/or specific groups of people."),
+  p("These features include:"),
+  tags$ul(
+    tags$li("How mental health is changing over specific periods of time."),
+    tags$li("When mental health is improving or worsening at the fastest rate (points of acceleration)."),
+    tags$li("How variable mental health responses are over time within individuals (stability).")
+  ),
+  p('To use this tool please read the "Instructions" tab on each page to guide you through the process. In brief the main aims of each page:'),
+  h4("Data Preparation"),
+  p("This allows the user to upload a wide format of their longitudinal dataset. Select which columns measure time and the phenotype they want to model trajectories on. Converts the dataframe to long format. Allows the user to download the long format dataset."),
+  h4("Data Exploration"),
+  p("This is the first stage of the trajectory modelling. Here the user either uploads a long format dataset or uses the dataset formatted on the previous page (Data Preparation). They specify the columns relatated to the variables to include in the model. There is a choice of model type and the user can see which model type looks like it best fits their data to explore further on the following pages."),
+  h4("Group Interactions"),
+  p("Split the trajectories by varaibles to examine the differences in trajectories."),
+  h4("Points of acceleration"),
+  p("Examine timing of peak velocity of trajectories. This feature highlights a critical period at which further support or interventions could be introduced to dramatically shift an individual’s illness trajectory."),
+  h4("Stability"),
+  p("Captures within-individual variability in depressive symptoms over time and compare how this varies by different forms of interventions or combinations of interventions.")
+)
+)
+
 format_page <- tabPanel(
   title = "Data Preparation",
   fluidPage(
@@ -48,6 +73,7 @@ ui <- navbarPage(
              ".shiny-output-error { visibility: hidden; }",
              ".shiny-output-error:before { visibility: hidden; }"
   ),
+  welcome_page,
   format_page,
   overview_page,
   intervention_page
--- inst/shinyApp/modules.R
+++ inst/shinyApp/modules.R
@@ -29,10 +29,28 @@ wide2longUI <- function(id, label = "wide2long") {
       )
     ),
     mainPanel(
-      tagList(
-        htmlOutput(ns("warningMsgEmpty")),
-        htmlOutput(ns("warningMsgLen")),
-        tableOutput(ns("preview"))
+      tabsetPanel(
+        tabPanel("Instructions",
+          tagList(
+          h2("Convert longitudinal data from wide format to long format"),
+          p('In order to model trajectories R requires the data frame to be in "long" format. It\'s likely that the data is initially inputted as a "wide" format. This page allows you to upload wide formatted data and converts it to long. Similar to the image displayed below.'),
+          img(src = "wide2long.png", height = 350, width = 550),
+          h4("Steps:"),
+          tags$ol(
+            tags$li('Upload a wide format csv or tsv file of your dataset. This must be less than 30 MB. Click on the "Output" tab and proceed with the following steps.'),
+            tags$li("Select which columns correspond to age and the phenotype you want to model, eg. depression, at each time point. Make sure that you select these in the correct chronological order and have the same corresponding time points for the phenotype and age."),
+            tags$li('Default names for the new columns are "age", "occ", "dep" and "dep_cat". You can change these in the text boxes provided if you wish.'),
+            tags$li('Now you can see a preview of the newly formatted long dataframe by clicking on the "Output" tab. You have the option to download it (in .csv format) and also use for analysis on the subsequent pages.')
+          ),
+          )
+        ),
+        tabPanel("Output",
+          tagList(
+          htmlOutput(ns("warningMsgEmpty")),
+          htmlOutput(ns("warningMsgLen")),
+          tableOutput(ns("preview"))
+          )
+        )
       )
     )
   )
@@ -49,9 +67,9 @@ wide2longServer <- function(id) {
         output$moreControls <- renderUI(
           tagList(
             selectInput(ns("ageCols"), "Select columns for age at each assessment:", choices = NULL, multiple = TRUE),
+            selectInput(ns("depCols"), "Select columns for depression scores at each assessment:", choices = NULL, multiple = TRUE),
             textInput(ns("age"), "Name of new column for age:", value = "age"),
             textInput(ns("occ"), "Name of new column for time point:", value = "occ"),
-            selectInput(ns("depCols"), "Select columns for depression scores at each assessment:", choices = NULL, multiple = TRUE),
             textInput(ns("dep"), "Name of new column for depression scores:", value = "dep"),
             textInput(ns("dep_cat"), "Name of new column for depression grouping/time point:", value = "dep_cat"),
             downloadButton(ns("downloadData"), "Download .csv")
