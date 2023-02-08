88fa2dce3a7d0009f94ed2f9701a6b2be7bc5bae
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2023-02-08 16:41:14 +0000

    changed conditional variable to a factor

--- R/modelCondPlotServer.R
+++ R/modelCondPlotServer.R
@@ -30,22 +30,43 @@ modelCondPlotServer <- function(id,
 
     # ---------------------------------------
     # paste the formula depending on whether covariates are wanted or not
-    modelDataEdit <- reactive({
-      # if(!covarsLogical()){
-      # fit <- lmer(formula = paste0(formCode(), "+ `", conditionVar(), "`"), REML=F , data = modelData())
-      # }else{
-      #   fit <- lmer(formula = paste0(formCode(), "+ `", conditionVar(), "`", " + ", paste0(covariates(), collapse = " + ") ), REML=F , data = modelData())
-      # }
+    fit <- reactive({
 
       if(modelType() == "Linear"){
-        fit <- lmer(formula = paste0(formCode(), "+ `", conditionVar(), "` + ", age(), "*", conditionVar() ), REML=F , data = modelData())
+        fit <- lmer(formula = paste0(formCode(),
+                                     "+ factor(", conditionVar(), ")",
+                                     " + ", age(), "*factor(", conditionVar(), ")",
+                                     ),
+                    REML=F , data = modelData())
       } else if(modelType() == "Quadratic"){
-        fit <- lmer(formula = paste0(formCode(), "+ `", conditionVar(), "` + ", age(), "*", conditionVar(), " + I(", age(), "^2)*", conditionVar() ), REML=F , data = modelData())
+        fit <- lmer(formula = paste0(formCode(),
+                                     "+ factor(", conditionVar(), ")",
+                                     " + ", age(), "*factor(", conditionVar(), ")",
+                                     " + I(", age(), "^2)*factor(", conditionVar(), ")"
+                                     ),
+                    REML=F , data = modelData())
       } else if(modelType() == "Cubic"){
-        fit <- lmer(formula = paste0(formCode(), "+ `", conditionVar(), "` + ", age(), "*", conditionVar(), " + I(", age(), "^2)*", conditionVar()), REML=F , data = modelData())
+        fit <- lmer(formula = paste0(formCode(),
+                                     "+ factor(", conditionVar(), ")",
+                                     " + ", age(), "*factor(", conditionVar(), ")",
+                                     " + I(", age(), "^2)*factor(", conditionVar(), ")",
+                                     " + I(", age(), "^3)*factor(", conditionVar(), ")"
+                                     ),
+                    REML=F , data = modelData())
       } else if(modelType() == "Quartic"){
-        fit <- lmer(formula = paste0(formCode(), "+ `", conditionVar(), "` + ", age(), "*", conditionVar(), " + I(", age(), "^2)*", conditionVar()), REML=F , data = modelData())
+        fit <- lmer(formula = paste0(formCode(),
+                                     "+ factor(", conditionVar(), ")",
+                                     " + ", age(), "*factor(", conditionVar(), ")",
+                                     " + I(", age(), "^2)*factor(", conditionVar(), ")",
+                                     " + I(", age(), "^3)*factor(", conditionVar(), ")",
+                                     " + I(", age(), "^4)*factor(", conditionVar(), ")"
+                                     ),
+                    REML=F , data = modelData())
       }
+      return(fit)
+    })
+
+    modelDataEdit <- reactive({
 
       # select the index for the column that the user wants to split the analysis on
       i <- which(colnames(modelData()) %in% conditionVar())
@@ -53,32 +74,32 @@ modelCondPlotServer <- function(id,
       # add the "predicted" column to this dataset (it's not really a prediction because its the same dataset, it just shows the model)
       # add a column for coloring the plot by the split by variable
       modelDataEdit <- modelData() %>%
-        mutate(pred = predict(fit, ., re.form = NA)) %>%
+        mutate(pred = predict(fit(), ., re.form = NA)) %>%
         mutate(Group_Level = .[[i]] )
 
+      return(modelDataEdit)
+    })
       # ---------------
       # model results
       output$modelStatsFixed <- renderTable(
         cbind(
-          tidy(fit, "fixed"),
-          confint(fit, "beta_", method = "Wald")) %>%
+          tidy(fit(), "fixed"),
+          confint(fit(), "beta_", method = "Wald")) %>%
           mutate(p.z = 2 * (1 - pnorm(abs(statistic)))) %>%
           mutate(p.z = ifelse(p.z < 0.001, "p < 0.001", p.z))
       )
 
       output$modelStatsRandom <- renderTable(
-        as.data.frame(VarCorr(fit),
+        as.data.frame(VarCorr(fit()),
                       order = "lower.tri")
       )
 
       # ---------------------------------------
       # Paste the model formula for the user to see (don't want it to appear straight away - could improve this)
       output$form<- renderText({
-        paste0("<b>Model Formula:</b> ",  gsub(".*formula = (.+) , data =.*", "\\1", summary(fit)$call)[2])
-      })
+        paste0("<b>Model Formula:</b> ",  gsub(".*formula = (.+) , data =.*", "\\1", summary(fit())$call)[2])
 
-      return(modelDataEdit)
-    })
+      })
 
     # ---------------------------------------
     # Plot the split by variable plot
@@ -88,7 +109,7 @@ modelCondPlotServer <- function(id,
         geom_point()+
         geom_line() +
         geom_errorbar(aes(ymin = lower, ymax = upper)) +
-        geom_line(data = modelDataEdit(), aes(x= !!sym(age()) ,  y = pred, color = factor(Group_Level) ) , na.rm=T)+
+        geom_line(data = modelDataEdit(), aes(x= age_original ,  y = pred, color = factor(Group_Level) ) , na.rm=T)+
         scale_colour_discrete(na.translate = F)
     })
   }
