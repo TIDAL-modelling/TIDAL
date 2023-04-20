a9fb3c455f2ae6b6a2b9b7f63e9193dedac7a664
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2023-04-20 11:53:38 +0100

    change options to open in browser

--- R/launchTIDAL.R
+++ R/launchTIDAL.R
@@ -2,18 +2,25 @@
 #'
 #' Opens an interactive Shiny GUI
 #'
+#' "display" can be either "default" which opens as a pop out window in R studio or "browser" which opens in the browser.
 #'
 #'
 #' @import shiny
 #' @import shinythemes
 #'
 #' @export
-launchTIDAL <- function() {
+launchTIDAL <- function(display = "default") {
   appDir <- system.file("TIDALapp", package = "TIDAL")
+
   if (appDir == "") {
     stop("Could not find directory. Try re-installing `TIDAL`.", call. = FALSE)
+  } else {
+    if (display == "default") {
+      shiny::runApp(paste0(appDir, "/app.R"), display.mode = "normal")
+    } else if (display == "browser") {
+      shiny::runApp(paste0(appDir, "/app.R"), launch.browser = TRUE)
+    } else {
+      stop("Invalid value for 'display'. Please choose either 'default' or 'browser'.", call. = FALSE)
+    }
   }
-
-  shiny::runApp(paste0(appDir, "/app.R") , display.mode = "normal")
 }
-
