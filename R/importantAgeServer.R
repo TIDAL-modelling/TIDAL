a40e3e61a4c0b6ec4061e77b6e06b061cb73ab00
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2023-06-05 16:39:37 +0100

    remove peak velocity code for now

--- R/importantAgeServer.R
+++ R/importantAgeServer.R
@@ -72,18 +72,6 @@ importantAgeServer <- function(id,
             filter(term %in% paste0("I(",age(),"^3)")) %>%
             pull(estimate)
 
-          # Age at increased velocity (peak velocity)
-          velPeak <- -2 * b_age_2 / (3 * b_age_3) + mean(pull(modelDataEdit(), age_original))
-          velMin <- 2 * b_age_2 / (3 * b_age_3) + mean(pull(modelDataEdit(), age_original))
-
-          # Velocity as a new col
-          vel <- b_age +
-            ( 2 * b_age_2 * pull(modelDataEdit(), !!sym(age())) ) +
-            ( 3 * b_age_3 * (pull(modelDataEdit(), !!sym(age())))^2 )
-
-          modelDataEdit2 <- modelDataEdit() %>%
-            mutate(vel = vel)
-
           # Age at maximum symptoms
           maxSymptoms <- (-2*b_age_2 - (sqrt(4*(b_age_2^2) - (12*(b_age_3*b_age))))) / (6*b_age_3) + mean(pull(modelDataEdit(), age_original))
 
@@ -96,114 +84,12 @@ importantAgeServer <- function(id,
           return(list(
             velPeak = velPeak,
             velMin = velMin,
-            modelDataEdit2 = modelDataEdit2,
             maxSymptoms = maxSymptoms,
             maxSymY = maxSymY
           ))
 
-        } else if(modelType() == "Quartic"){
-          b_intercept <- tidy(modelFit(), "fixed") %>%
-            filter(term %in% "(Intercept)") %>%
-            pull(estimate)
-
-          b_age <- tidy(modelFit(), "fixed") %>%
-            filter(term %in% age()) %>%
-            pull(estimate)
-
-          b_age_2 <- tidy(modelFit(), "fixed") %>%
-            filter(term %in% paste0("I(",age(),"^2)")) %>%
-            pull(estimate)
-
-          b_age_3 <- tidy(modelFit(), "fixed") %>%
-            filter(term %in% paste0("I(",age(),"^3)")) %>%
-            pull(estimate)
-
-          b_age_4 <- tidy(modelFit(), "fixed") %>%
-            filter(term %in% paste0("I(",age(),"^4)")) %>%
-            pull(estimate)
-
-          # Age at increased velocity (peak velocity)
-          velPeak <- -6 * b_age_3 - sqrt((6 * b_age_3)^2 - 4 * (12 * b_age_4) * (2 * b_age_2)) / (24 * b_age_4) + mean(pull(modelDataEdit(), age_original))
-
-          # Age at decreased velocity
-          velMin <- -6 * b_age_3 + sqrt((6 * b_age_3)^2 - 4 * (12 * b_age_4) * (2 * b_age_2)) / (24 * b_age_4) + mean(pull(modelDataEdit(), age_original))
-
-          # Velocity as a new col
-            vel <- b_age +
-            ( 2 * b_age_2 * pull(modelDataEdit(), !!sym(age())) ) +
-            ( 3 * b_age_3 * (pull(modelDataEdit(), !!sym(age())))^2 ) +
-            ( 4 * b_age_4 * (pull(modelDataEdit(), !!sym(age())))^3 )
-
-          modelDataEdit2 <- modelDataEdit() %>%
-              mutate(vel = vel)
-
-          return(list(
-            velPeak = velPeak,
-            velMin = velMin,
-            modelDataEdit2 = modelDataEdit2
-          ))
-        }
-
-      })
-
-      output$peakVelText <- renderText({
-        req(modelType())
-        if(modelType() == "Linear"){
-        "Age at peak velocity cannot be calculated for linear models."
-        } else if(modelType() == "Quadratic"){
-        "Age at peak velocity cannot be calculated for quadratic models."
-        } else if(modelType() == "Cubic" | modelType() == "Quartic"){
-          range <- modelDataEdit() %>%
-            pull(age_original)
-          if(valuesList()$velPeak > max(range) | valuesList()$velMin < min(range) ){
-            "The peak velocities are outside the data range and therefore cannot be estimated for this model type."
-          }else{
-            paste0("Age at peak velocity: ", round(valuesList()$velMin, 2), " and ", round(valuesList()$velPeak, 2))
-          }
         }
       })
-
-      output$peakVelPlot <- renderPlot({
-        req(modelType())
-
-        if(modelType() == "Cubic" | modelType() == "Quartic"){
-          range <- modelDataEdit() %>%
-            pull(age_original)
-          if(valuesList()$velPeak > max(range) | valuesList()$velMin < min(range)  ){
-            ggplot() + theme_void()
-          }else{
-            ggplot(modelDataEdit()) +
-              geom_line(aes(x= age_original ,  y = pred), na.rm=T, linewidth = 0.75) +
-              geom_vline(xintercept = valuesList()$velMin, colour="red", linetype = "longdash") +
-              geom_vline(xintercept = valuesList()$velPeak, colour="red", linetype = "longdash") +
-              theme(text = element_text(size = 20), panel.background = element_rect(fill = "white"),
-                    axis.line = element_line(colour = "grey50")) +
-              ylab("Score") +
-              xlab("Age")
-          }
-        }
-      })
-
-      output$velPlot <- renderPlot({
-        req(modelType())
-        if(modelType() == "Cubic" | modelType() == "Quartic"){
-          range <- modelDataEdit() %>%
-            pull(age_original)
-          if(valuesList()$velPeak > max(range) | valuesList()$velMin < min(range)  ){
-            ggplot() + theme_void()
-          }else{
-            ggplot(valuesList()$modelDataEdit2) +
-              geom_line(aes(x= age_original, y = vel), color = "blue") +
-              geom_vline(xintercept =  valuesList()$velMin, colour="red", linetype = "longdash") +
-              geom_vline(xintercept = valuesList()$velPeak, colour="red", linetype = "longdash") +
-              theme(text = element_text(size = 20), panel.background = element_rect(fill = "white"),
-                    axis.line = element_line(colour = "grey50")) +
-              ylab("Velocity") +
-              xlab("Age")
-          }
-        }
-      })
-
 
       output$maxSymText <- renderText({
         req(modelType())
--- R/importantAgeUI.R
+++ R/importantAgeUI.R
@@ -17,10 +17,6 @@ importantAgeUI <- function(id, label = "Model") {
   ns <- NS(id)
 
   tabPanel("Output",
-  fluidRow(column(12, h4("Age at Peak Velocity"))),
-  fluidRow(column(12, textOutput(ns("peakVelText")))),
-  fluidRow(column(12, align="center", plotOutput(ns("peakVelPlot"), width = "50%", height = 300))),
-  fluidRow(column(12, align="center", plotOutput(ns("velPlot"), width = "50%", height = 300))),
   fluidRow(column(12, h4("Age at Maximum Symptoms"))),
   fluidRow(column(12, textOutput(ns("maxSymText")))),
   fluidRow(column(12, align="center", plotOutput(ns("maxSymPlot"), width = "50%", height = 300)))
