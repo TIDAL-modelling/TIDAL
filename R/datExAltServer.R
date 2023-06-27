5d9da25535fbf954a71524e8c54813b7025fc802
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2023-06-27 17:28:31 +0100

    use glht to calc CIs for data exploration score at ages

--- R/datExAltServer.R
+++ R/datExAltServer.R
@@ -40,50 +40,109 @@ datExAltServer <- function(id,
 
       # ------------------------------------------
       # Calculate the score at a given age (intercept + slope etc)
-      score <- reactive({
+      # use glht to calculate scores at ages
+      score_glht <- reactive({
         ageOrig <- modelDataEdit() %>% pull(age_original)
         ageOrig <- ageOrig[!is.na(ageOrig)]
 
-        score <- sapply(as.numeric(input$ageInput), function(x){
+        score <- lapply(as.numeric(input$ageInput), function(x){
+
+          rowNames <- rownames(summary(modelFit())$coefficients)
+
+          ageInput <- round(x - mean(ageOrig), 3)
+          ageInput2 <- ageInput^2
+          ageInput3 <- ageInput^3
+          ageInput4 <- ageInput^4
+          # --------------------
+          # Change for model type
           if(modelType() == "Linear"){
-            (summary(modelFit())$coefficients[1,1] +
-            (x - mean(ageOrig)) * summary(modelFit())$coefficients[2,1]) %>%
-              round(2)
-          } else if(modelType() == "Quadratic"){
-           ( summary(modelFit())$coefficients[1,1] +
-              (x - mean(ageOrig)) * summary(modelFit())$coefficients[2,1] +
-              (x - mean(ageOrig))^2 * summary(modelFit())$coefficients[3,1]) %>%
-              round(2)
+            res <- multcomp::glht(modelFit(), linfct = c( paste0(rowNames[1], " + ", rowNames[2], "*", ageInput, " == 0")) )
+          }else if(modelType() == "Quadratic"){
+
+
+            res <- glht(modelFit(), linfct = c( paste0(rowNames[1], " + ",
+                                                  rowNames[2], "*", ageInput," + \`",
+                                                  rowNames[3], "\`*", ageInput2,
+                                                  " == 0")) )
           } else if(modelType() == "Cubic"){
-            (summary(modelFit())$coefficients[1,1] +
-              (x - mean(ageOrig)) * summary(modelFit())$coefficients[2,1] +
-              (x - mean(ageOrig))^2 * summary(modelFit())$coefficients[3,1] +
-            (x - mean(ageOrig))^3 * summary(modelFit())$coefficients[4,1] )%>%
-              round(2)
+
+
+            res <- glht(modelFit(), linfct = c( paste0(rowNames[1], " + ",
+                                                  rowNames[2], "*", ageInput," + \`",
+                                                  rowNames[3], "\`*", ageInput2,  " + \`",
+                                                  rowNames[4], "\`*", ageInput3,
+                                                  " == 0") ) )
+
           } else if(modelType() == "Quartic"){
-           ( summary(modelFit())$coefficients[1,1] +
-              (x - mean(ageOrig)) * summary(modelFit())$coefficients[2,1] +
-              (x - mean(ageOrig))^2 * summary(modelFit())$coefficients[3,1] +
-            (x - mean(ageOrig))^3 * summary(modelFit())$coefficients[4,1] +
-            (x - mean(ageOrig))^4 * summary(modelFit())$coefficients[5,1]) %>%
-              round(2)
+
+
+            res <- glht(modelFit(), linfct = c( paste0(rowNames[1], " + ",
+                                                  rowNames[2], "*", ageInput," + \`",
+                                                  rowNames[3], "\`*", ageInput2,  " + \`",
+                                                  rowNames[4], "\`*", ageInput3,  " + \`",
+                                                  rowNames[5], "\`*", ageInput4,
+                                                  " == 0") ))
           }
+
+          # --------------------
+          res <- tidy(confint(res))
+
+          rowname <- paste0("Score (", traj(), ")")
+
+          res <-  res %>%
+            mutate(contrast = paste0(rowname, " (95% CIs)")) %>%
+            column_to_rownames(var = "contrast") %>%
+            mutate(across(where(is.numeric), round, 2))
+
+          return( res )
         })
         return(score)
+      })
+
+      # ------------------------------------------
+      output$test <- renderText({
+        paste0(score_glht)
       })
 
       # ------------------------------------------
       # Plot the score at the given age
       # points of intersection of age and score
       plotScoreAll <- eventReactive(input$ageInput, {
-        points <- data.frame(x = as.numeric(input$ageInput),
-                             y = as.numeric(score()))
+
+        req(score_glht())
+
+        estimate <- lapply(score_glht(), function(df) {
+          df %>%
+            dplyr::select(estimate)
+        })  %>% do.call(cbind, .)
+        colnames(estimate) <- input$ageInput
+        estimate <- estimate %>%
+          gather(age, score, 1:ncol(estimate)) %>%
+          mutate(age = as.numeric(age))
+
+        conf.low <- lapply(score_glht(), function(df) {df %>% dplyr::select(conf.low)}) %>% do.call(cbind, .)
+        colnames(conf.low) <- input$ageInput
+        conf.low  <- conf.low %>%
+          gather(age, conf.low, 1:ncol(conf.low)) %>%
+          mutate(age = as.numeric(age))
+
+        conf.high <- lapply(score_glht(), function(df) {df %>% dplyr::select(conf.high)}) %>% do.call(cbind, .)
+        colnames(conf.high) <- input$ageInput
+        conf.high  <- conf.high %>%
+          gather(age, conf.high, 1:ncol(conf.high)) %>%
+          mutate(age = as.numeric(age))%>%
+          dplyr::select(-age)
+
+        conf <- cbind(conf.low, conf.high, "age")
 
         ggplot() +
-          geom_line(data = modelDataEdit(), aes(x= age_original ,  y = pred ) , na.rm=T) +
-          geom_point(data = points, aes(x = x, y = y), col = "#1D86C7", size = 5) +
+          geom_line(data = modelDataEdit(), aes(x= age_original ,  y = pred  ) , na.rm=T) +
+          theme(legend.text = element_text(color = "black")) +
+          geom_errorbar(data = conf, aes(x = age, ymin = conf.low, ymax = conf.high), width = 0.5) +
+          geom_point(data = estimate, aes(x = age, y = score), col = "#1D86C7", size = 5) +
           ylab(paste0("Score (", traj(), ")")) +
           xlab("Age")
+
       })
 
       output$plot <- renderPlot({
@@ -95,11 +154,17 @@ datExAltServer <- function(id,
       # --- Age | Score -----
       # Change "Score" to the actual column name from the dataframe - which the user previously specified
       output$table <- renderTable({
-        df <- t(data.frame( input$ageInput, score() ))
-        rowname <- paste0("Score (", traj(), ")")
-        rownames(df) <- c("Age", rowname)
-        df
-      }, colnames = FALSE, rownames = TRUE)
+        req(score_glht())
+
+        estimateCI <- lapply(score_glht(), function(df) {
+          df %>%
+            mutate(estimateCI = paste0(estimate, " (", conf.low, " - ", conf.high, ")")) %>%
+            dplyr::select(estimateCI)
+        })  %>% do.call(cbind, .)
+        colnames(estimateCI) <- input$ageInputScore
+        estimateCI
+
+      }, colnames = TRUE, rownames = TRUE)
       # ------------------------------------------
     }
   )
